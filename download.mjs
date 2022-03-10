94fb2a9263b5fa0ba3086af63571bdef92476e1b
Eduardo Parra <edue17@gmail.com>
2022-03-11 00:06:07 +0100

    Update download.mjs

--- download.mjs
+++ download.mjs
@@ -2,30 +2,35 @@ import fetch from 'node-fetch'
 import { readFile, writeFile } from './fileUtils.js'
 
 const FUEL_STATION_ID = process.env.FUEL_STATION_ID
+const FUEL_STATION_ID_2 = process.env.FUEL_STATION_ID_2
 const GEOPORTAL_URL = `https://geoportalgasolineras.es/rest/${FUEL_STATION_ID}/busquedaEstacionPrecio`
+const GEOPORTAL_URL_2 = `https://geoportalgasolineras.es/rest/${FUEL_STATION_ID_2}/busquedaEstacionPrecio`
 
 const date = new Intl.DateTimeFormat('es-ES', { month: 'numeric', day: 'numeric' }).format(Date.now())
 
-fetch(GEOPORTAL_URL, {headers: { 'Accept': ' application/json' }})
-.then(res => res.json())
-.then((stationData) => {
-    let dataSaved = readFile()
-    
-    if(dataSaved.dates.at(-1) === date){
-      const numElments = dataSaved.dates.length - 1
-      dataSaved.gasolina[numElments] = stationData.precioGasolina95E5
-      dataSaved.diesel[numElments] = stationData.precioGasoleoA
-    } else {
-      dataSaved.dates.push(date)
-      dataSaved.gasolina.push(stationData.precioGasolina95E5)
-      dataSaved.diesel.push(stationData.precioGasoleoA)
-    }
-    
-    writeFile(dataSaved)
+const download = (type) => {
+    fetch(GEOPORTAL_URL, {headers: { 'Accept': ' application/json' }})
+    .then(res => res.json())
+    .then((stationData) => {
+        let dataSaved = readFile()
 
-    console.log(`Guardado con fecha ${date}: gasolina: ${stationData.precioGasolina95E5} y gasoil: ${stationData.precioGasoleoA}`)
-    
-})
-  .catch(err => {
-    console.error(err)
-  })
+        if(dataSaved.dates.at(-1) === date){
+          const numElments = dataSaved.dates.length - 1
+          dataSaved[type][numElments] = stationData.precioGasolina95E5
+        } else {
+          dataSaved.dates.push(date)
+          dataSaved[type].push(stationData.precioGasolina95E5)
+        }
+
+        writeFile(dataSaved)
+
+        console.log(`Guardado con fecha ${date}: gasolina: ${stationData.precioGasolina95E5} y gasoil: ${stationData.precioGasoleoA}`)
+
+    })
+      .catch(err => {
+        console.error(err)
+      })
+}
+
+download('luna')
+download('institutos')
