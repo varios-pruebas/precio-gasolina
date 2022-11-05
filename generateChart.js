065984ee933ae9526e693c9557c761893832cd1d
Eduardo Parra <edue17@gmail.com>
2022-11-05 13:40:50 +0100

    Update generateChart.js

--- generateChart.js
+++ generateChart.js
@@ -2,8 +2,8 @@ import QuickChart from 'quickchart-js'
 
 export const generateChart = ( dataSaved, types ) => {
   const chart = new QuickChart();
-  const min = Math.trunc(Math.min(...[...dataSaved.luna, ...dataSaved.institutos]) * 100) / 100
-  const max = Math.max(...[...dataSaved.luna, ...dataSaved.institutos])
+  const min = Math.trunc(Math.min(...[...dataSaved.luna.slice(-250), ...dataSaved.institutos.slice(-250)]) * 100) / 100
+  const max = Math.max(...[...dataSaved.luna.slice(-250), ...dataSaved.institutos.slice(-250)])
 
   chart.setWidth(500)
   chart.setHeight(300);
@@ -11,11 +11,11 @@ export const generateChart = ( dataSaved, types ) => {
   chart.setConfig({
     type: 'line',
     data: {
-      labels: dataSaved.dates,
+      labels: dataSaved.dates.slice(-250),
       datasets: types.map(type => ({
           label: type,
           display: false,
-          data: dataSaved[type],
+          data: dataSaved[type].slice(-250),
           fill: true,
         }),
       ),
